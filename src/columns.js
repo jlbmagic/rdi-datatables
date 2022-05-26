51bd4df01e9b66ca7f409ebdededd2760f8264e6
Jeremy Brown <jlbmagic@gmail.com>
2022-05-26 09:09:51 -0400

    set up DT to work with 2 data sets.

---
+++ src/columns.js
@@ -0,0 +1,15 @@
+const customersColumns = [
+  { title: "Company", data: "fieldData.CompanyName" },
+  { title: "Address", data: "fieldData.StreetAddress" },
+  { title: "City", data: "fieldData.City" },
+  { title: "State", data: "fieldData.State" },
+  { title: "Zip Code", data: "fieldData.Zip" },
+];
+
+const admissionsColumns = [
+  { title: "Matric Year", data: "fieldData.MatricYear" },
+  { title: "Status", data: "fieldData.Status" },
+  { title: "Grade", data: "fieldData.Grade" },
+];
+
+export { customersColumns, admissionsColumns };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-//Here we're importing items we'll need. You can add other imports here.
-
+import { customersColumns, admissionsColumns } from "./columns";
 var table;
 
 // exposing loadData to FileMaker Script
 window.loadData = function (json) {
-  // TODO: Load the DataTables libraries by linking to the DataTables CDN.
-  //TODO: Get data from FileMaker
-  //TODO: prep it for the JS.
+  const obj = JSON.parse(json);
+  const data = obj.data;
+  const type = obj.type;
 
-  //TODO: create the columns.
+  const chooseColumns = function (type) {
+    return type === "Customers" ? customersColumns : admissionsColumns;
+  };
+  const columns = chooseColumns(type);
 
-  //TODO: Explore options of Datatables
-  //https://datatables.net/reference/option/
-  //https://datatables.net/examples/index
-  // Create the DataTable, after destroying it if already exists
-  table = $("#dtable").DataTable({
+  const options = {
     paging: true,
-    pageLength: 20,
+    pageLength: 25,
     searching: true,
     colReorder: true,
-    columns: [],
-    data: [],
-  });
+    columns: columns,
+    data: data,
+  };
+
+  table = $("#dtable").DataTable(options);
 };
